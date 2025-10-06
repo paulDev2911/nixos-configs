b0825849f138c0b06680b54dbea62c6ac81200ad
paulDev2911 <ph24311@tutamail.com>
2025-10-06 20:25:32 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let 
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        nixos = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ ./configuration.nix ];
+        };
+      };
+    };
+}

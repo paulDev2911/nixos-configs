14c6ad005a335a8895cb2e094871ad34c65a7128
paulDev2911 <ph24311@tutamail.com>
2025-11-27 17:40:03 +0100

    fixed salt.nix

--- modules/salt.nix
+++ modules/salt.nix
@@ -1,24 +1,18 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ 
-    salt_3006  # Verwende stabile Version statt neueste
-  ];
+  environment.systemPackages = with pkgs; [ salt ];
 
-  services.salt.master = {
-    enable = true;
-    package = pkgs.salt_3006;
-  };
-  
-  services.salt.minion = {
-    enable = true;
-    package = pkgs.salt_3006;
-  };
+  # Temporär deaktiviert wegen Crypto-Bug
+  services.salt.master.enable = false;
+  services.salt.minion.enable = false;
 
   networking.firewall.allowedTCPPorts = [ 4505 4506 ];
 
   systemd.tmpfiles.rules = [
     "d /srv/salt 0755 root root -"
     "d /srv/pillar 0700 root root -"
+    "d /etc/salt/pki/master 0755 root root -"
+    "d /etc/salt/pki/minion 0755 root root -"
   ];
 }
